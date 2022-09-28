09a1f61b5e1140811cb222d443e5af58cb04f698
Cho Changhun <cvcvcx@naver.com>
2022-09-28 19:53:20 +0900

    5-2-4
    5-2-4

---
+++ LeranReactNavigation/screens/HomeScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Button} from 'react-native';
+
+function HomeScreen({navigation}) {
+  return (
+    <View>
+      <Button
+        title="Detail1 열기"
+        onPress={() => navigation.navigate('Detail', {id: 1})}
+      />
+      <Button
+        title="Detail2 열기"
+        onPress={() => navigation.navigate('Detail', {id: 2})}
+      />
+      <Button
+        title="Detail3 열기"
+        onPress={() => navigation.navigate('Detail', {id: 3})}
+      />
+    </View>
+  );
+}
+export default HomeScreen;
