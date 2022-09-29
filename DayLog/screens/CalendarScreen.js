8b257b8b1905a51c65ec2989bbb79cd489c0c4a2
Cho Changhun <cvcvcx@naver.com>
2022-09-29 20:08:47 +0900

    Context
    Context를 사용하여 Context내에 있는 children에서 공통된 값을 전달할 수 있게 함
    createContext를 하면 Provider라는 것을 생성하게 되는데, 그 Context.Provider내의 value값을 공유하게됨

---
+++ DayLog/screens/CalendarScreen.js
@@ -0,0 +1,12 @@
+import React, {useContext} from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+import LogContext from '../contexts/LogContext';
+
+function CalendarScreen() {
+  return <View style={styles.block} />;
+}
+
+const styles = StyleSheet.create({
+  block: {},
+});
+export default CalendarScreen;
---
+++ DayLog/screens/FeedsScreen.js
@@ -0,0 +1,11 @@
+import React, {useContext} from 'react';
+import {StyleSheet, View, TextInput} from 'react-native';
+import LogContext from '../contexts/LogContext';
+function FeedsScreen() {
+  return <View style={styles.block} />;
+}
+
+const styles = StyleSheet.create({
+  block: {},
+});
+export default FeedsScreen;
